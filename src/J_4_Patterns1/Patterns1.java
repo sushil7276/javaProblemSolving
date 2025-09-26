fdf92fdfe678c8471cb5d9892a1e11c5399cee23
Prasad Karmalkar <prasadkarmalkar2020@gmail.com>
2025-09-26 11:32:39 +0530

    Triangle Number Pattern

--- src/J_4_Patterns1/Patterns1.java
+++ src/J_4_Patterns1/Patterns1.java
@@ -75,11 +75,58 @@ public class Patterns1 {
         *****
         ******
      */
-    public static void tringleStarPattern(int n){
+    public static void tringleStarPattern(int n) {
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+    Problem statement
+        Print the following pattern for the given N number of rows.
+
+        Pattern for N = 4
+        1
+        22
+        333
+        4444
+        Detailed explanation ( Input/output format, Notes, Images )
+        Constraints
+        0 <= N <= 50
+        Sample Input 1:
+        5
+        Sample Output 1:
+        1
+        22
+        333
+        4444
+        55555
+        Sample Input 2:
+        6
+        Sample Output 2:
+        1
+        22
+        333
+        4444
+        55555
+        666666
+     */
+    public static void triangleNumberPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        squarePattern(7);
+//        squarePattern(7);
+//        tringleStarPattern(5);
+        triangleNumberPattern(5);
     }
 }
