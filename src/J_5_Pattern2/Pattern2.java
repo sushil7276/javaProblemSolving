67d964552a041dbdc56c6b66cb135b3f0d31da33
Prasad Karmalkar <prasadkarmalkar2020@gmail.com>
2025-09-29 13:32:25 +0530

    Pattern 2 complete and assignment start

--- src/J_5_Pattern2/Pattern2.java
+++ src/J_5_Pattern2/Pattern2.java
@@ -112,9 +112,114 @@ public class Pattern2 {
         }
     }
 
+    /*
+    Problem statement
+        Print the following pattern for the given number of rows.
+
+        The dots represent spaces.
+
+        Detailed explanation ( Input/output format, Notes, Images )
+        Constraints :
+        0 <= N <= 50
+        Sample Input 1:
+        5
+        Sample Output 1:
+                   1
+                  232
+                 34543
+                4567654
+               567898765
+        Sample Input 2:
+        4
+        Sample Output 2:
+                   1
+                  232
+                 34543
+                4567654
+     */
+    public static void trangleOfNumbers(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < i * 2; j++) {
+                System.out.print(j);
+            }
+            for (int k = i * 2 - 2; k >= i; k--) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+    Problem statement
+        Print the following pattern for the given number of rows.
+        Note: N is always odd.
+
+        The dots represent spaces.
+
+        Detailed explanation ( Input/output format, Notes, Images )
+        Constraints :
+        1 <= N <= 49
+        Sample Input 1:
+        5
+        Sample Output 1:
+          *
+         ***
+        *****
+         ***
+          *
+        Sample Input 2:
+        3
+        Sample Output 2:
+          *
+         ***
+          *
+     */
+    public static void diamondStarts(int n) {
+        for (int i = 1; i <= n; i++) {
+            int space = Math.abs((n / 2) - i + 1);
+            int start = n - 2 * space;
+            for (int s = 1; s <= space; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= start; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void diamondStarts2(int n) {
+        int row = (n / 2) + 1;
+        for (int i = 1; i <= row; i++) {
+            for (int s = 1; s <= row - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= row - 1; i++) {
+            for (int s = 1; s <= i; s++) {
+                System.out.print(" ");
+            }
+            int star = (2 * (row - i)) - 1;
+            for (int j = 1; j <= star; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 //        mirrorNumberPattern(4);
 //        invertedNumberPattern(4);
-        starPattern(4);
+//        starPattern(4);
+//        trangleOfNumbers(5);
+//        diamondStarts(3);
+        diamondStarts2(5);
     }
 }
---
+++ src/J_5_Pattern2/Pattern2Assignment.java
@@ -0,0 +1,134 @@
+package J_5_Pattern2;
+
+public class Pattern2Assignment {
+    /*
+    Problem statement
+        Write a program to print N number of rows for Half Diamond pattern using stars and numbers
+
+        Note : There are no spaces between the characters in a single line.
+
+        Detailed explanation ( Input/output format, Notes, Images )
+        Constraints :
+        0 <= N <= 50
+        Sample Input 1 :
+        3
+        Sample Output 1 :
+        *
+        *1*
+        *121*
+        *12321*
+        *121*
+        *1*
+        *
+        Sample Input 2 :
+         5
+        Sample Output 2 :
+        *
+        *1*
+        *121*
+        *12321*
+        *1234321*
+        *123454321*
+        *1234321*
+        *12321*
+        *121*
+        *1*
+        *
+     */
+    public static void halfDiamondPattern(int n) {
+        int row = n * 2 - 1;
+
+        System.out.println("*");
+        for (int i = 1; i <= row; i++) {
+            int col = i > n ? (n * 2) - i : i;
+            System.out.print("*");
+            for (int j = 1; j <= col; j++) {
+                System.out.print(j);
+            }
+            for (int j = col - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.print("*");
+            System.out.println();
+        }
+        System.out.println("*");
+    }
+
+    /*
+    Problem statement
+        Write a program to print parallelogram pattern for the given N number of rows.
+        The dots represent spaces.
+        Detailed explanation ( Input/output format, Notes, Images )
+        Constraints :
+        0 <= N <= 50
+        Sample Input 1 :
+        3
+        Sample Output 1 :
+        ***
+         ***
+          ***
+        Sample Input 2 :
+        5
+        Sample Output 2 :
+        *****
+         *****
+          *****
+           *****
+            *****
+     */
+    public static void parallelogramPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s < i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+    Problem statement
+        Write a program to print triangle of user defined integers sum.
+
+        Detailed explanation ( Input/output format, Notes, Images )
+        Constraints :
+        0 <= N <= 50
+        Sample Input 1 :
+        3
+        Sample Output 1 :
+        1=1
+        1+2=3
+        1+2+3=6
+        Sample Input 2 :
+         5
+        Sample Output 2 :
+        1=1
+        1+2=3
+        1+2+3=6
+        1+2+3+4=10
+        1+2+3+4+5=15
+     */
+    public static void sumPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = 1; j <= i; j++) {
+                if (j == i) {
+                    System.out.print(j + "=");
+                } else {
+                    System.out.print(j + "+");
+                }
+                sum += j;
+            }
+            System.out.print(sum);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+//        halfDiamondPattern(5);
+//        parallelogramPattern(5);
+        sumPattern(5);
+    }
+}
